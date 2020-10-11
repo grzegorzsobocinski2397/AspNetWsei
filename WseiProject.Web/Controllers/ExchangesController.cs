a1e2234c8f7a266f7ddd928fe0122899781518f6
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2020-10-11 17:49:12 +0200

    Add first view and controller

---
+++ WseiProject.Web/Controllers/ExchangesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WseiProject.Web.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
