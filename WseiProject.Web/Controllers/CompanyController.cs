02162ffd7f68e98f0521f839266c88d93b9561fa
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2020-10-21 19:06:21 +0200

    Create Company controller

---
+++ WseiProject.Web/Controllers/CompanyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WseiProject.Web.Controllers
+{
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WseiProject.Web/Models/CompanyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WseiProject.Web.Models
+{
+    public class CompanyModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+    }
+
+}
