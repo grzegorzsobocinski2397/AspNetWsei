ad245bf6fe606e79b74960f62f0f213f92d6771d
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2020-11-04 20:53:30 +0100

    Create Tree Controller

---
+++ WseiProject.Web/Controllers/TreeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WseiProject.Web.Models;
+using WseiProject.Web.Models.Responses;
+
+namespace WseiProject.Web.Controllers
+{
+    [ApiController]
+    [Route("api/tree")]
+    public class TreeController : ControllerBase
+    {
+        /// <summary>
+        /// Adds new tree to the database.
+        /// </summary>
+        public AddNewItemResponse Post(TreeModel tree)
+        {
+            bool isValid = tree.IsValid();
+            AddNewItemResponse response = new AddNewItemResponse() { IsSuccess = isValid };
+            return response;
+        }
+    }
+}
---
+++ WseiProject.Web/Models/Responses/AddNewItemResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace WseiProject.Web.Models.Responses
+{
+
+    /// <summary>
+    /// Response object for the <see cref="CompanyAddController"/>.
+    /// </summary>
+    public class AddNewItemResponse
+    {
+        /// <summary>
+        /// Whether the add operation was successful.
+        /// </summary>
+        public bool IsSuccess { get; set; }
+    }
+}
