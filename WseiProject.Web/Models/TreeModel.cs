e06c4f822339859d8fc21d6951f968ab02b09350
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2020-11-04 20:52:44 +0100

    Create Tree Model

---
+++ WseiProject.Web/Models/TreeModel.cs
@@ -0,0 +1,32 @@
+﻿namespace WseiProject.Web.Models
+{
+    /// <summary>
+    /// Model of a tree from the forest.
+    /// </summary>
+    public class TreeModel
+    {
+        /// <summary>
+        /// Unique ID of a tree in the forest.
+        /// </summary>
+        public string ID { get; set; }
+        /// <summary>
+        /// Any special information about the tree.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Whether the tree has been already cut down by the lumberjack.
+        /// </summary>
+        public bool IsCutDown { get; set; }
+
+        /// <summary>
+        /// Whether the tree information is valid.
+        /// </summary>
+        public bool IsValid()
+        {
+            bool isNameValid = ID.Length > 0;
+            bool isDescriptionValid = Description.Length > 0;
+            return isNameValid && isDescriptionValid;
+        }
+    }
+}
