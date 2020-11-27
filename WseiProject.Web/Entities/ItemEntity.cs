f800a3d49eac9a0afd1ac89c651ba7f6519296ff
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2020-11-27 17:51:29 +0100

    Add item entity

---
+++ WseiProject.Web/Entities/ItemEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace WseiProject.Web.Entities
+{
+    public class ItemEntity
+    {
+        public int Id { get; set;}
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
