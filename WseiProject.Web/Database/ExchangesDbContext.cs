bbb7ae6c4050916d5b85e17ffbd23a85b9defbd6
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2020-11-27 18:02:01 +0100

    Add exchanges db context

---
+++ WseiProject.Web/Database/ExchangesDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WseiProject.Web.Database
+{
+    public class ExchangesDbContext
+    {
+    }
+}
--- WseiProject.Web/Startup.cs
+++ WseiProject.Web/Startup.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WseiProject.Web.Database;
 
 namespace WseiProject.Web
 {
@@ -23,6 +21,7 @@ namespace WseiProject.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ExchangesDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ExchangeThings"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
