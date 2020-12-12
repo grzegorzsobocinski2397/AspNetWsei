4264e90549b2d03579277589343a2baec6278462
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2020-12-12 13:32:18 +0100

    Add Latest Item View Component on the Home Page

---
+++ WseiProject.Web/ViewComponents/LatestItemViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WseiProject.Web.Database;
+
+namespace WseiProject.Web.ViewComponents
+{
+    public class LatestItemViewComponent : ViewComponent
+    {
+        private readonly ExchangesDbContext _dbContext;
+
+        public LatestItemViewComponent(ExchangesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var latestItem = _dbContext.Items.OrderByDescending(x => x.Id).First();
+
+            return View(latestItem);
+        }
+    }
+}
